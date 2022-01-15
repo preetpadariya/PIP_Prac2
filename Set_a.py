50d28fe2e8e5140a10d68c4190a7ca7da777adc6
preetpadariya <95370529+preetpadariya@users.noreply.github.com>
2022-01-15 15:00:37 +0530

    Set (a)

---
+++ Set_a.py
@@ -0,0 +1,12 @@
+"""Student Id = 20CE063
+    Student name = Preet Padariya"""
+# Write a Python program to add member(s) in a set and clear a set
+
+set = {0,10,100,'Preet','Meet',(1,2,3)}
+print(set)
+set.add('PYTHON') # for adding single element
+print(set)
+set.update(['1','PIP'])
+print(set)
+a=set.clear()
+print(a)
