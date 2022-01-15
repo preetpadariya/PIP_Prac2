7ccbe8a39215af8947db6ff746fc90ca572a6f0d
preetpadariya <95370529+preetpadariya@users.noreply.github.com>
2022-01-15 15:08:38 +0530

    Update Set_d.py

--- Set_d.py
+++ Set_d.py
@@ -1,1 +1,7 @@
-
+"""Student Id = 20CE063
+    Student name = Preet Padariya"""
+# Write a Python program to find maximum and the minimum value in a set.
+set = {1, 3, 5, 10, 100, 111}
+print(set)
+print("Maximum value :",max(set))
+print("Minimum value :",min(set))
