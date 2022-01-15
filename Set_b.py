a6ad8681583da35e185b75d7d847cb0164ea59c0
preetpadariya <95370529+preetpadariya@users.noreply.github.com>
2022-01-15 15:03:20 +0530

    Set (b)

---
+++ Set_b.py
@@ -0,0 +1,11 @@
+"""Student Id = 20CE063
+    Student name = Preet Padariya"""
+# Write a Python program to remove an item from a set if it is present in the set.
+set = {0,10,100,'Preet','Meet',(1,2,3)}
+print(set)
+#removing 10 from set
+set.remove(10)
+print(set)
+#removing Meet from set
+set.remove('Meet')
+print(set)
