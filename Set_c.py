8c2c83cccb0d6795c5a660a993c0bb3ed7507802
preetpadariya <95370529+preetpadariya@users.noreply.github.com>
2022-01-15 15:06:20 +0530

    Set (c)

---
+++ Set_c.py
@@ -0,0 +1,9 @@
+"""Student Id = 20CE063
+    Student name = Preet Padariya"""
+# Write a Python program to create an intersection, Union, difference of sets.
+a = {1,2,3,4,5,6,7}
+b = {4,5,6,7,8,10,11}
+
+print("A U B = ", a.union(b))
+print("A ∩ B = ", a.intersection(b))
+print("A - B = ", a.difference(b))
