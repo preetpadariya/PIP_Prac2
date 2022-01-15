89e46e915763db7f8028534a4a16c694b3bbac99
preetpadariya <95370529+preetpadariya@users.noreply.github.com>
2022-01-15 15:18:56 +0530

    Tuple (c)

---
+++ Tuple_c.py
@@ -0,0 +1,16 @@
+"""Student Id = 20CE063
+    Student name = Preet Padariya"""
+# Write a Python program to add an item in a tuple.
+
+# tuple
+a=(10,20,60,70)
+print(a)
+# List
+b=list(a)
+print(b)
+# list add one item
+b.append(100)
+print(b)
+# list to tuple...
+c=tuple(b)
+print(c)
