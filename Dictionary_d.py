24ca7e9c0c22b393646f6e3d766e3f0612d3f5c1
preetpadariya <95370529+preetpadariya@users.noreply.github.com>
2022-01-15 14:49:25 +0530

    Dictionary (d)

---
+++ Dictionary_d.py
@@ -0,0 +1,12 @@
+"""Student Id = 20CE063
+    Student name = Preet Padariya"""
+# Write a Python script to add a key to a dictionary.
+Disk1 = {
+    0:10,
+    1:20,
+}
+print("Sample Dictionary : ")
+print(Disk1)
+print("Add a key to a dictionary :")
+((Disk1.update({2 : 30}))) #updates the dictionary by adding key-value pair.
+print(Disk1)
