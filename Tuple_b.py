ae43098490ea904cdaab42127fe95ccb152b36df
preetpadariya <95370529+preetpadariya@users.noreply.github.com>
2022-01-15 15:16:40 +0530

    Tuple (b)

---
+++ Tuple_b.py
@@ -0,0 +1,8 @@
+"""Student Id = 20CE063
+    Student name = Preet Padariya"""
+# Write a Python program to create a tuple with numbers and print one item.
+
+tpl = (0,1,2,3,4,5,6,7,8,9,10)
+print(tpl[0])
+print(tpl[4])
+print(tpl[7])
