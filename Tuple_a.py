06312a65f876e99b9f049254037451532d86f560
preetpadariya <95370529+preetpadariya@users.noreply.github.com>
2022-01-15 15:14:40 +0530

    Tuple (a)

---
+++ Tuple_a.py
@@ -0,0 +1,6 @@
+"""Student Id = 20CE063
+    Student name = Preet Padariya"""
+# Write a Python program to create a tuple with different data types.
+tupl = ("tuple",63,True,5.67)
+print(type(tupl))
+print(tupl)
