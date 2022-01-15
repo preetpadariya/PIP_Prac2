e1591e59486a1a4ac295c879cf914235b6fc9f70
preetpadariya <95370529+preetpadariya@users.noreply.github.com>
2022-01-15 15:21:15 +0530

    Tuple (d)

---
+++ Tuple_d.py
@@ -0,0 +1,6 @@
+"""Student Id = 20CE063
+    Student name = Preet Padariya"""
+# 4 Write a Python program to convert a tuple to a string.
+tpl = ('P', 'R', 'E', 'E', 'T', '2' , '0')
+str = ''.join(tpl)
+print(str)
